5e26eb8002e5eabc2d2f28a18565e49525148ccb
s_anno <s_anno@spica.co.jp>
2015-01-14 00:11:51 +0900

    Initial Commit

---
+++ URLRequestBackend/URLRequestBackend.h
@@ -0,0 +1,19 @@
+//
+//  URLRequestBackend.h
+//  URLRequestBackend
+//
+//  Created by 安野周太郎 on 2015/01/14.
+//  Copyright (c) 2015年 amo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLRequestBackend.
+FOUNDATION_EXPORT double URLRequestBackendVersionNumber;
+
+//! Project version string for URLRequestBackend.
+FOUNDATION_EXPORT const unsigned char URLRequestBackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLRequestBackend/PublicHeader.h>
+
+
