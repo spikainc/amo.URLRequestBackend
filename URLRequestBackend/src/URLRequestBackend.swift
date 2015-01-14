586abbb028f2bca05b2b9ce919d8d296784c3b87
s_anno <s_anno@spica.co.jp>
2015-01-14 21:50:28 +0900

    JSONParserPlugin 追加

--- URLRequestBackend/src/URLRequestBackend.swift
+++ URLRequestBackend/src/URLRequestBackend.swift
@@ -9,43 +9,29 @@
 import Foundation
 import Promise
 
-public typealias Response = (request: NSURLRequest, response: NSURLResponse, data: NSData)
-public typealias RequestHandler = (request: NSURLRequest) -> Promise<Response>
-
-public protocol Plugin {
-    var interceptRequest: ((request: NSMutableURLRequest) -> Promise<NSMutableURLRequest>)? { get }
-    var interceptRequestError: ((error: NSError) -> Promise<NSMutableURLRequest>)? { get }
-    var interceptResponse: ((response: Response) -> Promise<Response>)? { get }
-    var interceptResponseError: ((error: NSError) -> Promise<Response>)? { get }
-}
-
-public class PluginBase: Plugin {
-    public var interceptRequest: ((request: NSMutableURLRequest) -> Promise<NSMutableURLRequest>)?
-    public var interceptRequestError: ((error: NSError) -> Promise<NSMutableURLRequest>)?
-    public var interceptResponse: ((response: Response) -> Promise<Response>)?
-    public var interceptResponseError: ((error: NSError) -> Promise<Response>)?
-}
+public typealias Result = (request: NSURLRequest, response: NSURLResponse, data: AnyObject?)
+public typealias RequestHandler = (request: NSURLRequest) -> Promise<Result>
 
 public class Manager {
     private let requestHandler: RequestHandler
-    private var plugins = [Plugin]()
+    private var plugins = [PluginProtocol]()
     
     public class var sharedInstance: Manager {
         struct Shared {
-            static let instance = Manager(HTTPRequestHandler)
+            static let instance = Manager()
         }
         return Shared.instance
     }
     
-    public init(requestHandler: RequestHandler) {
+    public init(requestHandler: RequestHandler = HTTPRequestHandler) {
         self.requestHandler = requestHandler
     }
     
-    public func addPlugin(plugin: Plugin) {
+    public func addPlugin(plugin: PluginProtocol) {
         self.plugins.append(plugin)
     }
     
-    public func request(request: NSURLRequest) -> Promise<Response> {
+    public func request(request: NSURLRequest) -> Promise<Result> {
         var mutableRequest = request.mutableCopy() as NSMutableURLRequest
         var requestPromise = Promise<NSMutableURLRequest>.resolve(mutableRequest)
         for plugin in self.plugins {
@@ -60,11 +46,11 @@ public class Manager {
         var responsePromise = requestPromise.then(self.requestHandler)
         
         for plugin in reverse(self.plugins) {
-            if let intercept = plugin.interceptResponse {
+            if let intercept = plugin.interceptResult {
                 responsePromise = responsePromise.then(intercept)
             }
             
-            if let intercept = plugin.interceptResponseError {
+            if let intercept = plugin.interceptResultError {
                 responsePromise = responsePromise.catch(intercept)
             }
         }
@@ -73,8 +59,8 @@ public class Manager {
     }
 }
 
-public func HTTPRequestHandler(request: NSURLRequest) -> Promise<Response> {
-    return Promise<Response>({ (deferred) -> () in
+public func HTTPRequestHandler(request: NSURLRequest) -> Promise<Result> {
+    return Promise<Result>({ (deferred) -> () in
         let queue = NSOperationQueue()
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response, data, error) -> Void in
             if error == nil {
@@ -84,5 +70,55 @@ public func HTTPRequestHandler(request: NSURLRequest) -> Promise<Response> {
             }
         })
     })
+}
+
+public protocol PluginProtocol {
+    var interceptRequest: ((request: NSMutableURLRequest) -> Promise<NSMutableURLRequest>)? { get }
+    var interceptRequestError: ((error: NSError) -> Promise<NSMutableURLRequest>)? { get }
+    var interceptResult: ((result: Result) -> Promise<Result>)? { get }
+    var interceptResultError: ((error: NSError) -> Promise<Result>)? { get }
+}
+
+public struct Plugin {
+    public class Base: PluginProtocol {
+        public init() {}
+        public var interceptRequest: ((request: NSMutableURLRequest) -> Promise<NSMutableURLRequest>)?
+        public var interceptRequestError: ((error: NSError) -> Promise<NSMutableURLRequest>)?
+        public var interceptResult: ((result: Result) -> Promise<Result>)?
+        public var interceptResultError: ((error: NSError) -> Promise<Result>)?
+    }
+}
+
+extension Plugin {
+    public class JsonParser: Base {
+        let option: NSJSONReadingOptions
+        public init(option: NSJSONReadingOptions = .AllowFragments) {
+            self.option = option
+            super.init()
+            self.interceptResult = self._interceptResult
+        }
+        
+        func _interceptResult(result: Result) -> Promise<Result> {
+            if let data = result.data? as? NSData {
+                let (json: AnyObject?, error) = self.parse(data)
+                if let e = error {
+                    return Promise<Result>.reject(e)
+                }
+                return Promise<Result>.resolve((result.request, result.response, json))
+            }
+            return Promise<Result>.resolve(result)
+        }
+        
+        public func parse(data: NSData!) -> (AnyObject?, NSError?) {
+            if data == nil || data.length == 0 {
+                return (nil, nil)
+            }
+            
+            var error: NSError?
+            let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: &error)
+            
+            return (json, error)
+        }
+    }
 }
 
--- URLRequestBackendTests/URLRequestBackendTests.swift
+++ URLRequestBackendTests/URLRequestBackendTests.swift
@@ -26,3 +26,20 @@ class URLRequestBackendTests: XCTestCase {
         XCTAssertTrue(URLRequestBackend.Manager.sharedInstance === URLRequestBackend.Manager.sharedInstance)
     }
 }
+
+class JSONParserPluginTests: XCTestCase {
+    var plugin: URLRequestBackend.Plugin.JsonParser!
+    
+    override func setUp() {
+        super.setUp()
+        self.plugin = URLRequestBackend.Plugin.JsonParser()
+    }
+    
+    func test_parseを使ってJSONをparseできる() {
+        let body = "{\"a\": \"b\"}"
+        let data = body.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)!
+        let (json: AnyObject?, error) = self.plugin.parse(data)
+        XCTAssertNotNil(json)
+        XCTAssertEqual("b", json?["a"] as String)
+    }
+}
