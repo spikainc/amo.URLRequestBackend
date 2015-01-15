03682f6ae5ea81758ddcb3bc609b75355e2a069c
s_anno <s_anno@spica.co.jp>
2015-01-15 18:03:17 +0900

    テスト追加

--- URLRequestBackendTests/tests/UtilTests.swift
+++ URLRequestBackendTests/tests/UtilTests.swift
@@ -29,13 +29,49 @@ class UtilTests: XCTestCase {
             (["a": ["b": "c", "d": "e"]], "a[b]=c&a[d]=e"),
             (["a": ["b": ["c", "d"]]], "a[b][]=c&a[b][]=d"),
             ([" !\"#$%&'()*+,/:;<=>?@[\\]^`{|}": " !\"#$%&'()*+,/:;<=>?@[\\]^`{|}"], "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E%60%7B%7C%7D=%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2F%3A%3B%3C%3D%3E%3F%40%5B%5C%5D%5E%60%7B%7C%7D"),
+            ([
+                "str": "b",
+                "num": 100,
+                "strnum": "100",
+                "array": ["a", "b", "c"],
+                "dict": ["hoge": "fuga", "fizz": "buzz"],
+                "recursive": [
+                    "str": "b",
+                    "num": 100,
+                    "strnum": "100",
+                    "array": ["a", "b", "c"],
+                    "dict": ["hoge": "fuga", "fizz": "buzz"],
+                ],
+                "recursiveArray": [
+                    [
+                        "str": "b",
+                        "num": 100,
+                        "strnum": "100",
+                        "array": ["a", "b", "c"],
+                        "dict": ["hoge": "fuga", "fizz": "buzz"],
+                    ], [
+                        "str": "b",
+                        "num": 100,
+                        "strnum": "100",
+                        "array": ["a", "b", "c"],
+                        "dict": ["hoge": "fuga", "fizz": "buzz"],
+                    ], [
+                        "str": "b",
+                        "num": 100,
+                        "strnum": "100",
+                        "array": ["a", "b", "c"],
+                        "dict": ["hoge": "fuga", "fizz": "buzz"],
+                    ],
+                ]
+                ], "str=b&num=100&strnum=100&array[]=a&array[]=b&array[]=c&dict[hoge]=fuga&dict[fizz]=buzz&recursive[str]=b&recursive[num]=100&recursive[strnum]=100&recursive[array][]=a&recursive[array][]=b&recursive[array][]=c&recursive[dict][hoge]=fuga&recursive[dict][fizz]=buzz&recursiveArray[][str]=b&recursiveArray[][num]=100&recursiveArray[][strnum]=100&recursiveArray[][array][]=a&recursiveArray[][array][]=b&recursiveArray[][array][]=c&recursiveArray[][dict][hoge]=fuga&recursiveArray[][dict][fizz]=buzz&recursiveArray[][str]=b&recursiveArray[][num]=100&recursiveArray[][strnum]=100&recursiveArray[][array][]=a&recursiveArray[][array][]=b&recursiveArray[][array][]=c&recursiveArray[][dict][hoge]=fuga&recursiveArray[][dict][fizz]=buzz&recursiveArray[][str]=b&recursiveArray[][num]=100&recursiveArray[][strnum]=100&recursiveArray[][array][]=a&recursiveArray[][array][]=b&recursiveArray[][array][]=c&recursiveArray[][dict][hoge]=fuga&recursiveArray[][dict][fizz]=buzz"),
         ]
     }
     func test_parametersで渡した値がURL内にクエリとして入る() {
         for params in self.parametersで渡した値がURL内にクエリとして入るDataProvider() {
             let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:8000/")!)
+            let expected = params.1
             let actual = URLRequestBackend.Util.escapeParameters(params.0)
-            XCTAssertEqual(params.1, actual)
+            XCTAssertEqual(sorted((split(expected, { $0 == "&"}))), sorted((split(actual, { $0 == "&"}))))
         }
     }
 }
